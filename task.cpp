49fa5f486ba3e8a15ed35145f561252484e99eab
Garrett Quintero <garrett.quintero@gmail.com>
2018-09-22 19:01:15 -0600

    initial commit with task

---
+++ task.cpp
@@ -0,0 +1,5 @@
+//
+// Created by quintero on 9/22/18.
+//
+
+#include "task.h"
---
+++ task.h
@@ -0,0 +1,21 @@
+//
+// Created by quintero on 9/22/18.
+//
+
+#ifndef ALGOBOWL_TASK_H
+#define ALGOBOWL_TASK_H
+
+#include <string>
+using namespace std;
+
+class task {
+public:
+    int run_times [3];
+    int call_times[3];
+    int available_time;
+
+    string print();
+};
+
+
+#endif //ALGOBOWL_TASK_H
